2e28ef536e84ef1f7f93f86507ae3938e0bf0219
JanezRadescek <janez.radescek@gmail.com>
2019-01-20 21:59:23 +0100

    Update demo_evaluateTaylor.m

--- seminarska/demo_evaluateTaylor.m
+++ seminarska/demo_evaluateTaylor.m
@@ -1,14 +1,33 @@
 %demo
 
 %koeficienti taylorja glede na 
-a = [00 01 02;
-    10 11 NaN;
-    20 NaN NaN];
+a = [1 2 3;
+    4 5 NaN;
+    6 NaN NaN];
 
-u = 1;
-v = 2;
+d = 5;
+a = NaN(d);
 
-p = evaluatePoly(a,u,v);
+for i = 1:d
+    for j = 1:d+1-i
+        a(i,j) = mod(i*j*7,d)-d/2;
+    end
+end
+        
 
+u = linspace(-1,1);
+v = linspace(-1,1);
+p = NaN(100,100);
 
+tic;
+
+for i = 1:100
+    for j = 1:100 
+        p(i,j) = evaluatePoly(a,u(i),v(j));
+    end
+end
+
+time = toc
+
+surf(u,v,p);
 
